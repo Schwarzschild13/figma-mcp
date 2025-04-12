b4788d5578e4c372acf947adfc927e31ff152a18
Harikrishnan <harikrishnan.pr.ht@billdesk.com>
2025-04-13 04:39:16 +0530

    fix ui html bundle issue

--- vite.config.ui.ts
+++ vite.config.ui.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { createRequire } from "node:module";
+const require = createRequire(import.meta.url);
+const { viteSingleFile } = require("vite-plugin-singlefile"); // ✅ fix here
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    viteSingleFile(), // ✅ plugin works now
+  ],
+  base: "./",
+  build: {
+    outDir: "dist",
+    emptyOutDir: false,
+    rollupOptions: {
+      input: "ui.html",
+    },
+  },
+});
